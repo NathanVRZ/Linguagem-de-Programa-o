551e0a448fddb4dc830bc1aa49c3961634c4b049
Nathan Lino <nata.silva@bandtec.com.br>
2020-10-24 21:27:16 -0300

    push do ap oshi

---
+++ maven_turma_2/src/main/java/com/bandtec/maven_turma_2/Components.java
@@ -0,0 +1,12 @@
+package com.bandtec.maven_turma_2;
+
+import oshi.SystemInfo;
+import oshi.hardware.HardwareAbstractionLayer;
+
+public class Components {
+        SystemInfo si = new SystemInfo();
+        HardwareAbstractionLayer hw = si.getHardware();
+        
+        
+        
+}
